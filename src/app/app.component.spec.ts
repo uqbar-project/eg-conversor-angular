606c8a8e1dd564ec50edae25f5767e28d9288125
Fernando Dodino <fernando.dodino@gmail.com>
2019-07-14 22:49:29 -0300

    Upgrade to Angular 8.1.1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,6 +29,10 @@ describe('Tests de AppComponent', () => {
     expect(componente.title).toEqual('Conversor')
   }))
   it('debe aparecer el título Conversor Angular en un tag h1', async(() => {
+    // Este test busca un h1 => 
+    //   h1 no tiene indicaciones semánticas para la aplicación => no es "el título"
+    //   entonces si el cliente nos pide un cambio que involucra pasar de h1 a h2
+    //   no solo fallará este test sino todos los que busquen un elemento h1
     const compiled = fixture.debugElement.nativeElement
     expect(compiled.querySelector('h1').textContent).toContain('Conversor Angular')
   }))
@@ -38,9 +42,9 @@ describe('Tests de AppComponent', () => {
     conversor.convertir()
     fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement
-    expect(compiled.querySelector('p').textContent).toContain('160,934')
+    expect(compiled.querySelector('#kilometros').textContent).toContain('160,934')
   }))
-  it('al convertir millas a kilómetros desde la vista debe dejar en un p la conversión en kilómetros y 3 decimales con coma', async(() => {
+  it('al convertir millas a kilómetros desde la vista debe mostrar la conversión en kilómetros en 3 decimales con coma', async(() => {
     // Simulo la carga de 100 millas en el input type text (no puedo pasarle un entero, tiene que ser un texto)
     // querySelector permite encontrar un elemento del DOM por css, por id con #, por clase con . y por tipo de elemento sin prefijo
     // como vimos arriba : https://www.w3schools.com/jsref/met_document_queryselector.asp
