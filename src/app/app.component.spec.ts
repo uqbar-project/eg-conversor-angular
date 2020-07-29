7fe7e51e8f85f27ea9ac4dc108e73694588577b3
Fernando Dodino <fernando.dodino@gmail.com>
2020-07-29 20:28:30 -0300

    Enhancing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,60 +1,47 @@
-import { TestBed, async } from '@angular/core/testing'
+import { async, TestBed } from '@angular/core/testing'
+
 import { AppComponent } from './app.component'
+import { importsConversor } from './app.module'
 
 /** Imports extras tomados de @NgModule */
-import { importsConversor } from './app.module';
-
 describe('Tests de AppComponent', () => {
   let appComponent
   let componente
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({ declarations: [AppComponent], imports: importsConversor }).compileComponents()
     appComponent = TestBed.createComponent(AppComponent)
     componente = appComponent.debugElement.componentInstance
   }))
+
   it('debe crear correctamente la aplicación', async(() => {
     expect(componente).toBeTruthy()
   }))
-  it(`debe tener como título 'Conversor'`, async(() => {
-    expect(componente.title).toEqual('Conversor')
+
+  it('el título debe ser Conversor Angular', async(() => {
+    // No buscamos un tag html h1, ni un span, ni nada que tenga que ver con cuestiones
+    // estéticas, lo que semánticamente es un título lo podemos buscar mediante un tag
+    // específico para testear, el `data-testid` que se llame título
+    expect(buscarElemento('titulo').textContent).toContain('Conversor Angular')
   }))
-  it('debe aparecer el título Conversor Angular en un tag h1', async(() => {
-    // Este test busca un h1 =>   h1 no tiene indicaciones semánticas para la
-    // aplicación => no es "el título"   entonces si el cliente nos pide un cambio
-    // que involucra pasar de h1 a h2   no solo fallará este test sino todos los que
-    // busquen un elemento h1
-    const compiled = appComponent.debugElement.nativeElement
-    expect(compiled.querySelector('h1').textContent).toContain('Conversor Angular')
-  }))
+
   it('conversión de millas a kilómetros exitosa con 3 decimales', async(() => {
     componente.conversor.millas = 100
 
-    const convertirButton = appComponent.nativeElement.querySelector('[data-testid="convertir"')
+    const convertirButton = buscarElemento('convertir')
     convertirButton.click()
     appComponent.detectChanges()
 
     appComponent.whenStable().then(() => {
-      const compiled = appComponent.nativeElement
-      expect(compiled.querySelector('[data-testid="kilometros"]').textContent).toContain('160,934')
+      const resultado = buscarElemento('kilometros')
+      expect(resultado.textContent).toContain('160,934')
     })
   }))
-  // it('conversión de millas a kilómetros fallida - ingresa caracter alfabético', async(() => {
-  //   const millasInput: HTMLInputElement = appComponent.nativeElement.querySelector('[data-testid="millas"]')
-  //   millasInput.value = 'AAAA'
-  //   dispatchEvent(new Event('input'))
 
-  //   const convertirButton = appComponent
-  //     .nativeElement
-  //     .querySelector('[data-testid="convertir"]')
-  //   convertirButton.click()
-  //   appComponent.detectChanges()
-
-  //   appComponent
-  //     .whenStable()
-  //     .then(() => {
-  //       console.log(componente.conversor.kilometros)
-  //       expect(millasInput.value).toBe('AAAA')
-  //     })
-  // }))
-
+  /* Función auxiliar que permite buscar un elemento por data-testid */
+  const buscarElemento = (testId: string) => {
+    const compiled = appComponent.debugElement.nativeElement
+    return compiled.querySelector(`[data-testid="${testId}"]`)
+  }
 })
+
